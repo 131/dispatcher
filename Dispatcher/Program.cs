e783d3a767e15991635fe8a38c691196608e43fe
Francois Leurent <131.php@cloudyks.org>
2013-09-27 07:07:02 +0200

    Usefull stuffs

--- Dispatcher/Program.cs
+++ Dispatcher/Program.cs
@@ -17,7 +17,7 @@ namespace Dispatcher
     static void Main()
     {
       ConsoleEx.AllocConsole();
-      ConsoleEx.AttachConsole(Process.GetCurrentProcess().Id);
+      ConsoleEx.AttachConsole(-1);
       string dispatched_cmd = Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]);
 
       var argsStart = Environment.CommandLine.IndexOf(" ", dispatched_cmd.Length);
@@ -47,12 +47,93 @@ namespace Dispatcher
       }
 
       if (cmd.Length == 0)
+      {
+        Console.Error.WriteLine("Cannot resolve cmd");
         return;
+      }
 
       string exepath = Path.GetFullPath(Path.Combine(dispatcher_dir, cmd[1]));
+
+      Console.Error.WriteLine("Calling " + exepath);
+
+
       string pargs = (cmd.Length > 2 ? cmd[2] + " " : "") + initargs;
-      RunProcess(exepath, pargs);
+      //RunProcess(exepath, pargs);
+      RunCmd(exepath, pargs);
     }
+
+
+    private static void RunCmd(string exePath, string args)
+    {
+
+      string exeDir = Path.GetDirectoryName(exePath);
+      Process proc = new Process();
+      proc.StartInfo.FileName = @"C:\Windows\system32\cmd.exe";
+
+      // Si l'on veut rediriger le sortie du process il faut que l'on utilse UseShellExecute = false
+      proc.StartInfo.UseShellExecute = false;
+      proc.StartInfo.CreateNoWindow = true;
+
+      proc.StartInfo.Arguments = "/c \"" + exePath + " "+ args + "\"";
+      //some likes to be in the path (aka rsync)
+
+      Console.WriteLine("Calling cmd with " + proc.StartInfo.Arguments);
+
+      if (false)
+      {
+        proc.StartInfo.CreateNoWindow = true;
+        proc.StartInfo.UseShellExecute = false;
+        proc.Start();
+        proc.WaitForExit();
+        return;
+      }
+
+      proc.StartInfo.RedirectStandardOutput = true;
+      proc.StartInfo.RedirectStandardInput = true;
+      proc.StartInfo.RedirectStandardError = true;
+
+      //some likes to be in the path (aka rsync)
+      proc.StartInfo.EnvironmentVariables["PATH"] = Environment.GetEnvironmentVariable("PATH") + ";" + exeDir;
+
+
+      proc.Start();
+      ConsoleEx.AttachConsole(proc.Id);
+
+
+
+      Stream instrm = Console.OpenStandardInput();
+      Stream outstrm = Console.OpenStandardOutput();
+      Stream errstrm = Console.OpenStandardError();
+
+
+      var job = new Job();
+      job.AddProcess(Process.GetCurrentProcess().Handle);
+      job.AddProcess(proc.Handle);
+
+      Chat error = Chat.Start(proc.StandardOutput.BaseStream, outstrm, ChatMethod.Sync, "pout > cout");
+      Chat output = Chat.Start(proc.StandardError.BaseStream, errstrm, ChatMethod.Sync, "perr > cout");
+      Chat io = Chat.Start(instrm, proc.StandardInput.BaseStream, ChatMethod.Async, "cin > pin");
+
+
+      if (ConsoleEx.IsInputRedirected && ConsoleEx.IsInputDisk)
+      {
+        while (!io.EOF) ;
+        io.Close();
+      }
+
+
+      proc.WaitForExit();
+
+      error.End();
+      output.End();
+      io.End();
+
+      while (error.Alive || output.Alive) ;
+
+
+      Environment.Exit(proc.ExitCode);
+    }
+
 
     private static void RunProcess(string exepath, string pargs)
     {
