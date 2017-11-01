4605d20a01f61c132172b2ccb4005ed31445e646
Francois Leurent <131.php@cloudyks.org>
2017-11-01 12:45:38 +0100

    Use System.configuration instead of custom file format

--- Dispatcher/Program.cs
+++ Dispatcher/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Configuration;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Utils;
@@ -14,13 +15,12 @@ namespace Dispatcher
     private static string args;
     private static bool use_showwindow;
 
-    const string FLAG_DISPATCHER_GUI = "DISPATCHER_GUI"; //toggle !use_showwindow
+    const string FLAG_USE_SHOWWINDOW = "USE_SHOWWINDOW";
 
     static void Main()
     {
 
       Dictionary<string, string> envs = new Dictionary<string, string>();
-
       if (!ExtractCommandLine(out exePath, out args, out use_showwindow, envs))
         Environment.Exit(1);
 
@@ -28,7 +28,6 @@ namespace Dispatcher
       envs["Path"] =  Environment.GetEnvironmentVariable("PATH") + ";" + exeDir;
       foreach(KeyValuePair<string, string> env in envs)
         Environment.SetEnvironmentVariable(env.Key,  env.Value);
-
 
       var pInfo = new PROCESS_INFORMATION();
       var sInfoEx = new STARTUPINFOEX();
@@ -74,67 +73,51 @@ namespace Dispatcher
       Environment.Exit((int) exitCode);
     }
 
-
-    private static bool ExtractCommandLine(out string exePath, out string args, out bool use_showwindow, Dictionary<string, string> envs)
-    {
-      args = exePath = String.Empty;
+        private static bool ExtractCommandLine(out string exePath, out string args, out bool use_showwindow, Dictionary<string, string> envs)
+        {
+            args = exePath = String.Empty;
 #if DISPACHER_WIN
       use_showwindow = false;
 #else
       use_showwindow = true;
 #endif
+            exePath = ConfigurationManager.AppSettings["PATH"];
 
+            if (ConfigurationManager.AppSettings[FLAG_USE_SHOWWINDOW] != null)
+                use_showwindow = !(ConfigurationManager.AppSettings[FLAG_USE_SHOWWINDOW] == "0" || ConfigurationManager.AppSettings[FLAG_USE_SHOWWINDOW] == "");
 
-      string dispatched_cmd = Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]);
+            if (String.IsNullOrEmpty(exePath))
+            {
+                Console.Error.WriteLine("Cannot resolve cmd");
+                return false;
+            }
+            
+            string dispatched_cmd = Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]);
 
-      var argsStart = Environment.CommandLine.IndexOf(" ", dispatched_cmd.Length);
-      args = argsStart != -1 ? Environment.CommandLine.Substring(argsStart + 1) : "";
+            string dispatcher = Assembly.GetExecutingAssembly().Location;
+            string dispatcher_dir = Path.GetDirectoryName(dispatcher);
 
-      string dispatcher = Assembly.GetExecutingAssembly().Location;
-      string dispatcher_dir = Path.GetDirectoryName(dispatcher);
-      string text = System.IO.File.ReadAllText(dispatcher_dir + "\\dispatch");
+            exePath = Path.GetFullPath(Path.Combine(dispatcher_dir, exePath));
 
-      var cmd = new string[0];
-      Dictionary<string, string> replaces = new Dictionary<string, string>
-      {
-        {"%dwd%", dispatcher_dir},
-      };
+            Dictionary<string, string> replaces = new Dictionary<string, string> {
+                {"%dwd%", dispatcher_dir},
+            };
 
-      string[] lines = text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-      for(var i=0;i<lines.Length; i++)
-      {
-        var line = Replace(lines[i], replaces);
-        var entry = line.Split(new string[] { " " }, 3, StringSplitOptions.None);
+            List<string> values = new List<string>();
+            foreach (string key in ConfigurationManager.AppSettings) {
+                string value = ConfigurationManager.AppSettings[key];
+                if (key.StartsWith("ARGV"))
+                    args += (Regex.IsMatch(value, "^[a-zA-Z0-9_./:^-]+$") ? value :  "\"" + value + "\"") + " ";
+                if (key.StartsWith("ENV_"))
+                    envs[key.Remove(0,4)] = Replace(value, replaces);
+            }
 
-        if (entry.Length < 2) continue;
-        if (dispatched_cmd == entry[0])
-        {
-          cmd = entry;
-          for (int j = i+1; j < lines.Length; j++)
-          {
-            Match e = Regex.Match(lines[j], @"^\s*([a-z_0-9-]+)\s*=\s*(.+)?", RegexOptions.IgnoreCase);
-            if(!e.Success)break;
-
-            if(e.Groups[1].Value == FLAG_DISPATCHER_GUI)
-              use_showwindow = false;
-            else envs[e.Groups[1].Value] = Replace(e.Groups[2].Value, replaces);
-          }
-
-          break;
+            var argsStart = Environment.CommandLine.IndexOf(" ", dispatched_cmd.Length);
+            if(argsStart != -1)
+                args += Environment.CommandLine.Substring(argsStart + 1);
+            
+            return true;
         }
-      }
-
-      if (cmd.Length == 0)
-      {
-        Console.Error.WriteLine("Cannot resolve cmd");
-        return false;
-      }
-
-      args = (cmd.Length > 2 ? cmd[2] + " " : "") + args;
-      exePath = Path.GetFullPath(Path.Combine(dispatcher_dir, cmd[1]));
-      return true;
-    }
-
     public static string Replace(string str, Dictionary<string, string> dict)
     {
       foreach (KeyValuePair<string, string> replacement in dict)
